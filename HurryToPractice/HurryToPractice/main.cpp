a881a8c86c29defea5e42ec0a7f38926a9499f3e
liyang <2629223794@qq.com>
2022-01-19 00:49:53 +0800

    GoogleTest初始化运行通过

--- HurryToPractice/HurryToPractice/main.cpp
+++ HurryToPractice/HurryToPractice/main.cpp
@@ -1,4 +1,21 @@
-int main()
+#include<iostream>
+#include<gtest/gtest.h>
+
+int add(int a, int b)
 {
+	return a + b;
+}
+
+TEST(testCase, test0)
+{
+	EXPECT_EQ(add(2, 3), 6);
+}
+
+int main(int argc,char** argv)
+{
+	testing::InitGoogleTest(&argc, argv);
+	RUN_ALL_TESTS();
+
+	getchar();
 	return 0;
 }
